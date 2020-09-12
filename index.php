754b0dd22d5064295dce55685fea90185dd18d17
TRSONGIR <67713900+TRSONGIR@users.noreply.github.com>
2020-09-13 00:13:55 +0430

    master

--- index.php
+++ index.php
@@ -1,37 +1,62 @@
-<!DOCTYPE html>
-<html lang="en" >
-<head>
-  <meta charset="UTF-8">
-  <title>Turk Series | سریال ترکی</title>
-  <link rel="stylesheet" href="./style.css">
-  <script type="text/javascript">
-    (function(){
-    var now = new Date();
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.async = true;
-    var script_address = 'https://cdn.yektanet.com/js/TURKSERIESDL.ML/native-TURKSERIESDL.ML-12695.js';
-    script.src = script_address + '?v=' + now.getFullYear().toString() + '0' + now.getMonth() + '0' + now.getDate() + '0' + now.getHours();
-    head.appendChild(script);
-    })();
-   </script>
-</head>
-<body>
-<div id="pos-article-display-12438"></div>
-<div id="pos-notification-12441"></div>
-<p>دانلود پس از 10 ثانیه به طور خوردکار شروع می شود</p>
-<!-- partial:index.partial.html -->
-<div id="timer_container">
-   <p id="time"></p>
- </div>
-<!-- partial -->
-  <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script><script  src="./script.js"></script>
 <?php
-$dlno = "http://dl1.turkseriesdl.ml/0:/";
-$shortener = $_GET ['url'];
-$url = $dlno.$shortener;
-header("Refresh: 10;url=$url"); 
+$a= "header("Refresh: 10;url=http://www.20script.ir/");";
+$b= $_GET['url'];
+$c= $a.$b;
+header("Refresh: 17;url=$c");
 ?>
-<div id="pos-footer-sticky-12442"></div>
-</body>
-</html>
+<BODY>
+<style>
+#minutes, #seconds{
+display: inline-block;
+background-color: #FF1493;
+border-radius: 10px;
+color: white;
+width: auto;
+  min-width:70px;
+margin-left: 10px;
+padding: 14px;
+}
+.holder
+{
+width: 100%;
+margin: auto;
+text-align: center; 
+}
+  #minutesTxt, #secondsTxt{
+  font-size: 27px;}
+</style>
+
+<script>   
+var given_seconds = 20; //fifteen seconds
+   
+setInterval(function() {
+ 
+    // Count down by one second
+    given_seconds=given_seconds-1;
+     
+    // Calculate hour/mins/sec's based on given seconds
+    hours = Math.floor(given_seconds / 3600);
+    minutes = Math.floor((given_seconds - (hours * 3600)) / 60);
+    seconds = given_seconds - (hours * 3600) - (minutes * 60);
+    
+      // Format the time with two digits per hour/mins/sec's
+   
+    minutesString = minutes.toString().padStart(2, '0') + '';
+    secondsString = seconds.toString().padStart(2, '0') + '';
+  
+  
+    // Print the current time so far
+    document.getElementById("minutesTxt").innerHTML = minutesString;
+	  document.getElementById("secondsTxt").innerHTML = secondsString;
+  
+    // Check if countdown is complete & redirect
+    if (given_seconds==1)
+      window.top.location='https://t.me/turkseriesdl/';
+
+}, 1000); // Update about every second
+</script>
+<div class="holder">
+  <div id="minutes" style="display:none"><span id="minutesTxt"></span><br>Minutes</div><div id="seconds"><span id="secondsTxt"></span><br>Seconds</div>  
+<br><br>
+</div>
+</BODY>
