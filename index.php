147ff88f9646370ec260849f4fd1ea946ad30933
TRSONGIR <67713900+TRSONGIR@users.noreply.github.com>
2020-09-12 21:06:05 +0430

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Turk Series | سریال ترکی</title>
+  <link rel="stylesheet" href="./style.css">
+  <script type="text/javascript">
+    (function(){
+    var now = new Date();
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.async = true;
+    var script_address = 'https://cdn.yektanet.com/js/TURKSERIESDL.ML/native-TURKSERIESDL.ML-12695.js';
+    script.src = script_address + '?v=' + now.getFullYear().toString() + '0' + now.getMonth() + '0' + now.getDate() + '0' + now.getHours();
+    head.appendChild(script);
+    })();
+   </script>
+</head>
+<body>
+<div id="pos-article-display-12438"></div>
+<div id="pos-notification-12441"></div>
+<p>دانلود پس از 10 ثانیه به طور خوردکار شروع می شود</p>
+<!-- partial:index.partial.html -->
+<div class="countdown">
+  <span class="countdown__number">10</span>
+  <svg class="countdown__icon" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 130 130" overflow="visible" enable-background="new 0 0 130 130">    
+    <path class="countdown__icon__circle" d="M5,64.8a59.8,59.8 0 1,0 119.6,0a59.8,59.8 0 1,0 -119.6,0"/>
+  </svg>
+</div>
+<?php
+$dlno = "http://dl1.turkseriesdl.ml/0:/";
+$shortener = $_GET ['url'];
+$url = $dlno.$shortener;
+header("Refresh: 10;url=$url"); 
+?>
+<!-- partial -->
+  <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.4/TweenMax.min.js'></script><script  src="./script.js"></script>
+<div id="pos-footer-sticky-12442"></div>
+</body>
+</html>
