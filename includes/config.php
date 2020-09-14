972ec50555250d523b8590b3569742f2a2ee2351
TRSONGIR <67713900+TRSONGIR@users.noreply.github.com>
2020-09-14 21:52:15 +0430

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -5,10 +5,10 @@
 // Turn off all error reporting
 error_reporting(0);
 $is_demo = "1";  // 1 for yes and 0 for no
-$dbhost  = "z8dl7f9kwf2g82re.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$dbuser  = "m6xc1keqq633ftr3"; // database user name
-$dbpass  = "jdmfohkspevzks9p"; // database password
-$dbname  = "eyaqp2lcvhyf4t20"; // database name
+$dbhost  = "sh4ob67ph9l80v61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$dbuser  = "vz4scdlmp7qm1xg8"; // database user name
+$dbpass  = "yy3jlvsfw58qiiss"; // database password
+$dbname  = "tb4047rmgcwr9zh8"; // database name
 if (empty($dbuser) || empty($dbname)) {
      die('connection to database error <br> please edit the includes/config.php <br> with the database details');
 }
